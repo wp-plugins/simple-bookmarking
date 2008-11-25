a7cb450cb731d59b8a02f8691d1f6459d9f9b649
thamizhchelvan <thamizhchelvan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-25 16:05:39 +0000

    adding php file
    git-svn-id: https://plugins.svn.wordpress.org/simple-bookmarking/trunk@75761 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-bookmark/simple-bookmark-sharing.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: Simple Bookmarking
+Plugin URI: http://www.phpbits.info/wordpress-plugins/wp-simple-bookmarking-plugin/
+Description: This is a simple social bookmarking sharing widget, which will be available in each post & pages with link icon to share your post and pages with popular social bookmarking engines.
+Author: Thamizhchelvan
+Version: 1.0
+Author URI: http://www.phpbits.info/
+*/
+
+
+function add_bookmark_icons($content)
+{
+
+if(is_page() || is_single())
+{
+//site & data
+global $post;
+$posttitle = urlencode($post->post_title);
+$posturl = urlencode(get_permalink($post->ID));
+$social_sites = array(
+"delicious" => "http://del.icio.us/post?url=".$posturl."&title=".$posttitle,
+"digg" => "http://digg.com/submit?phase=2&url=".$posturl."&title=".$posttitle,
+"reddit" => "http://reddit.com/submit?url=".$posturl."&title=".$posttitle,
+"facebook" => "http://www.facebook.com/share.php?u=".$posturl,
+"technorati" => "http://technorati.com/faves?add=".$posturl,
+"stumbleupon" => "http://www.stumbleupon.com/submit?url=".$posturl."&title=".$posttitle,
+"chatintamil" => "http://www.chatintamil.org/submit?url=".$posturl
+);
+
+//build the widget
+$bwidget = '<div>';
+$site_url = get_option('siteurl');
+
+foreach($social_sites as $site => $data)
+{
+
+$bwidget .= '<a rel="nofollow" target="_blank" title="'.$site.'" href="'.$data.'"><img src="'.$site_url.'/wp-content/plugins/simple-bookmark/images/'.$site.'.gif" alt="'.$site.'" ></a> | ';
+}
+
+$bwidget = substr($bwidget,0,strlen($bwidget)-4);
+$bwidget .= '</div>';
+$content .= $bwidget;
+}
+return $content;
+}
+
+add_action('the_content', 'add_bookmark_icons');
+
+?>
